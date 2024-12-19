0d2c2fe84869fe6917dd0b5dff4f718db503afc1
Vitalii Bodnar <bodnar.vietal@gmail.com>
2024-12-19 12:55:42 +0200

    flatlist of images

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { photos } from './data';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList 
+        data={photos}
+        numColumns={4}
+        contentContainerStyle={{ gap: 2 }}
+        columnWrapperStyle={{ gap: 2 }}
+        renderItem={({ item }) => <Image source={item.image} style={{ width: `${100 / 4}%`, aspectRatio: 1 }} />}
+      />
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
